392ce60a3698e297075edeae44b5485b2b2534f2
szt2otokov <szt2otokov@vasvari.org>
2023-09-21 13:16:43 +0200

    dun

---
+++ src/HelloOther.java
@@ -0,0 +1,7 @@
+public class HelloOther {
+    public static void main(String[] args) {
+        System.out.println("Hello, Patrick!");
+        System.out.println("Hello, Thomas!");
+        System.out.println("Hello, Peter!");
+    }
+}
